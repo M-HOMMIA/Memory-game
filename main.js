e47b106a2e73b7f76f977c6ee232a2def3ba4ecb
M-HOMMIA <128230795+M-HOMMIA@users.noreply.github.com>
2025-11-15 01:14:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,16 +6,29 @@ var compteur = 0,
 window.document.querySelector(".control-buttons span").onclick = function (){
     // Remove Splash Screen
     document.querySelector(".control-buttons").remove();
+    
+    // IMPORTANT: D'abord afficher les cartes, PUIS initialiser le jeu
     begin();
-    newG();
+    
+    // Attendre que l'animation soit terminée avant d'initialiser
+    setTimeout(function() {
+        newG();
+    }, 1000);
+    
     gagner = false;
 };	
 
 /*--------------------Begin Animation---------------------------*/
 function begin(){
     var gameBlock = document.getElementsByClassName('game-block');
-    $(gameBlock).hide().slideDown("slow");
-    document.getElementById('start').play();
+    $(gameBlock).show(); // Afficher directement sans animation pour debug
+    // $(gameBlock).hide().slideDown("slow"); // Animation désactivée temporairement
+    
+    // Jouer le son seulement s'il existe
+    var startAudio = document.getElementById('start');
+    if (startAudio) {
+        startAudio.play().catch(e => console.log('Audio play failed:', e));
+    }
 }
 
 /*--------------------New Game Button---------------------------*/	
@@ -27,8 +40,11 @@ window.document.getElementById('NewGame').onclick = function(){
     
     // Stop audio if playing
     if (gagner === true){
-        document.getElementById('yassamine').pause();
-        document.getElementById('yassamine').currentTime = 0;
+        var yassamineAudio = document.getElementById('yassamine');
+        if (yassamineAudio) {
+            yassamineAudio.pause();
+            yassamineAudio.currentTime = 0;
+        }
     }
     
     gagner = false;
@@ -46,7 +62,9 @@ window.document.getElementById('NewGame').onclick = function(){
     
     // Restart game
     begin();
-    newG();
+    setTimeout(function() {
+        newG();
+    }, 500);
 }
 
 /*--------------------Main Game Function---------------------------*/
@@ -60,6 +78,13 @@ function newG(){
     
     // Create Array From Game Blocks
     let blocks = Array.from(blocksContainer.children);
+    
+    console.log('Nombre de cartes:', blocks.length); // Debug
+    
+    if (blocks.length === 0) {
+        console.error('Aucune carte trouvée!');
+        return;
+    }
     
     // Create Range Of Keys
     let orderRange = Array.from(Array(blocks.length).keys());
@@ -84,6 +109,8 @@ function newG(){
         });
         
     });
+    
+    console.log('Jeu initialisé avec', blocks.length, 'cartes'); // Debug
     
     // Flip Block Function
     function flipBlock(selectedBlock) {
@@ -122,7 +149,11 @@ function newG(){
                     p.hide();
                     $(".memory-game-blocks").append(p);
                     p.slideDown("slow");
-                    document.getElementById('yassamine').play();
+                    
+                    var yassamineAudio = document.getElementById('yassamine');
+                    if (yassamineAudio) {
+                        yassamineAudio.play().catch(e => console.log('Audio play failed:', e));
+                    }
                 }, 500);
             }
             /*--------------------End Win Condition---------------------------*/
@@ -160,9 +191,13 @@ function newG(){
             firstBlock.classList.add('has-match');
             secondBlock.classList.add('has-match');
             
-            document.getElementById('success').play();
+            var successAudio = document.getElementById('success');
+            if (successAudio) {
+                successAudio.play().catch(e => console.log('Audio play failed:', e));
+            }
             /*--------------------Increment Counter---------------------------*/
             compteur++;
+            console.log('Paires trouvées:', compteur); // Debug
             /*--------------------End Increment---------------------------*/
         } else {
             
@@ -175,7 +210,10 @@ function newG(){
                 
             }, duration);
             
-            document.getElementById('fail').play();
+            var failAudio = document.getElementById('fail');
+            if (failAudio) {
+                failAudio.play().catch(e => console.log('Audio play failed:', e));
+            }
             
         }
         
